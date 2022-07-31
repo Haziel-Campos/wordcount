a483ed272735c2a4ba0fcf402f43b532f4c7b9f5
Haziel-Campos <109833115+Haziel-Campos@users.noreply.github.com>
2022-07-30 19:51:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var input = document.querySelector("#input")
+var wordsCount = document.querySelector("#word")
+var charCount = document.querySelector("#char")
+
+function getWords() {
+    var i = input.value
+    var c = i.split(' ').length;
+    charCount.innerHTML = i.length
+    if(i.length <= 0) return wordsCount.innerHTML = "0"
+    wordsCount.innerHTML = c.toString()
+}
